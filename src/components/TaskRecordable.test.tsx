ba1b27f103919a4b6d11677106dcb3cb4c0d784d
TeXmeijin <i8858garden@gmail.com>
2021-05-23 18:25:12 +0900

    add: @testing-library/react-nativeを入れてテストコード1つ

---
+++ src/components/TaskRecordable.test.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import TaskRecordable from './TaskRecordable';
+import { Task } from '../types/task';
+
+const mockTask: Task = {
+  id: 'id',
+  name: 'name',
+  last_checked: 'hoge',
+}
+
+test('form submits two answers', () => {
+  const { getByText } = render(
+    <TaskRecordable task={mockTask} />
+  );
+
+  const pressAble = getByText(mockTask.name)
+  expect(pressAble).toBeTruthy()
+  fireEvent.press(pressAble)
+});
